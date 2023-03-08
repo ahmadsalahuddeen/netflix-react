23335e22549e51d1ccee82868df8f0e943b4320c
ahmadsalahuddeen <ahmadsalahuddeen6017@gmail.com>
2023-03-08 19:10:04 +0530

    complete

--- netflix-react/src/components/RowPost/RowPost.js
+++ netflix-react/src/components/RowPost/RowPost.js
@@ -1,40 +1,65 @@
-import React, {useEffect, useState} from "react";
-import './RowPost.css'
-import {  imageUrl } from "../../constants/constants";
-import axios from '../../axios'
+import React, { useEffect, useState } from "react";
+import "./RowPost.css";
+import { imageUrl, API_KEY } from "../../constants/constants";
+import axios from "../../axios";
+import YouTube from "react-youtube";
 
 function RowPost(props) {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState('')
 
-    useEffect(() => {
-      
-    axios.get(props.url).then(response =>{
+  useEffect(() => {
+    axios
+      .get(props.url)
+      .then((response) => {
+        setMovies(response.data.results);
+      })
+      .catch((err) => {
+        alert("Network error");
+      });
+  }, []);
 
-setMovies(response.data.results)
-    }).catch(err => {
-        alert("Network error")
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovie = (id) => {
+
+    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response =>{
+console.log("rrrrrrrrrrrrrrrr", response);      
+        if (response.data.results.length !==0) {
+        setUrlId(response.data.results[0])
+       }else{
+        console.log('array empty');
+       }
+       
+    }).catch(err =>{
+        alert('no trailer')
     })
-    }, [])
+  };
 
-
-
-
-
-    
   return (
     <div className="row">
       <h2>{props.title}</h2>
 
       <div className="posters">
-        {movies.map((obj)=>{
-
-            return (
-        <img className={props.isSmall ? 'smallPoster' : 'poster'}
-         src={`${imageUrl+obj.backdrop_path}`} alt="" />
-            )    
-    })}
-        
+        {movies.map((obj) => {
+          return (
+            <img
+              onClick={() => handleMovie(obj.id)}
+              className={props.isSmall ? "smallPoster" : "poster"}
+              src={`${imageUrl + obj.backdrop_path}`}
+              alt=""
+            />
+          );
+        })}
       </div>
+    { urlId &&   <YouTube videoId={urlId.key} opts={opts} />}
     </div>
   );
 }
